94d599ccd30b780f0f5c6aea04baf0829fb8517b
Rafhael Milanes <rafhael.t3ch@gmail.com>
2024-11-23 19:28:44 -0300

    feat: add task item component

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./Tasks.scss";
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState([]);
+    const fetchTasks = async () => {
+        try {
+            const { data } = await axios.get(
+                "https://fsctaskmanagerbackend.onrender.com/tasks"
+            );
+            setTasks(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchTasks();
+    }, []);
+    return (
+        <>
+            <div className="tasks-container">
+                <h2>Minhas Tarefas</h2>
+                <div className="last-tasks">
+                    <h3>Últimas Tarefas</h3>
+                    <div className="tasks-list">
+                        {tasks
+                            .filter((task) => task.isCompleted === false)
+                            .map((lastTask) => (
+                                <p>{lastTask.description}</p>
+                            ))}
+                    </div>
+                </div>
+                <div className="completed-tasks">
+                    <h2>Tarefas Done</h2>
+                    <div className="tasks-list">
+                        {tasks
+                            .filter((task) => task.isCompleted === true)
+                            .map((completedtask) => (
+                                <p>{completedtask.description}</p>
+                            ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Tasks;
