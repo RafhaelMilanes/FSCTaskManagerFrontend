a7154f2a1aa4fb77c818a6bfa3f241488c584ed5
Rafhael Milanes <rafhael.t3ch@gmail.com>
2024-11-24 17:51:41 -0300

    feat: add SideBar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import { CustomButtom } from "./CustomButtom";
+
+import "./Sidebat.scss";
+import logo from "../assets/images/logo.png";
+
+const Sidbar = () => {
+    return (
+        <div className="sidebar-container">
+            <div className="logo">
+                <img src={logo} alt="Logo Full Stack Club " />
+            </div>
+
+            <div className="sign-out">
+                <CustomButtom>Sair</CustomButtom>
+            </div>
+        </div>
+    );
+};
+
+export default Sidbar;
