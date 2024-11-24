6eee7176c55ded1e95ca2ed490eb201a3c06235f
Rafhael Milanes <rafhael.t3ch@gmail.com>
2024-11-24 11:43:27 -0300

    feat: add CustomButton component

---
+++ src/components/CustomButtom.jsx
@@ -0,0 +1,12 @@
+import "./CustomButtom.scss";
+const CustomButtom = ({ onClick, children }) => {
+    return (
+        <>
+            <div className="custom-button-container" onClick={onClick}>
+                {children}
+            </div>
+        </>
+    );
+};
+
+export default CustomButtom;
